65a6fc5235ba46f51768d57f78be1076207fd471
kdjun97 <kdjun97@gmail.com>
2022-05-10 19:55:47 +0900

    Comment: Add functions' description 각 함수의 설명 추가

--- lib/posenet.dart
+++ lib/posenet.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:posenet/paint/painter.dart';
 import 'package:tflite/tflite.dart';
 import 'dart:io';
 import 'dart:math';
@@ -24,7 +25,8 @@ class _PoseNetPageState extends State<PoseNet>
   List posenetPoint;
   var res;
 
-  Future _getImage() async{
+  // Image를 받아오는 함수
+  Future _getImage() async {
     PickedFile image = (await _picker.getImage(source: ImageSource.gallery));
     if (this.mounted){
       setState(() {
@@ -44,19 +46,18 @@ class _PoseNetPageState extends State<PoseNet>
     loadModel(_image);
   }
 
-
+  // tflite Model Load
   void loadModel(PickedFile image) async
   {
     print('load model 진입');
 
-    if (res == null)
-    {
+    if (res == null) {
       res = await Tflite.loadModel(
         model: "assets/posenet_mv1_075_float_from_checkpoints.tflite",
         //labels: "assets/labels.txt"
       );
     }
-    try{
+    try {
       var result1 = await Tflite.runPoseNetOnImage(
           path: _image.path,     // required
           imageMean: 125.0,   // defaults to 125.0
@@ -69,19 +70,18 @@ class _PoseNetPageState extends State<PoseNet>
       setState(() {
         _recognitions = result1;
       });
-    } catch(e)
-    {
+    } catch(e) {
       print(e.message);
     }
   }
 
-  Widget backgr()
-  {
+  // Background image
+  Widget backgr() {
     return (_image == null) ? Text('d')
     : Image.file(File(_img.path));
   }
 
-
+  // point의 list를 반환해주는 함수
   List Keypoints(Size screen) {
     print('----------------KeyPoints 진입---------------');
     if (_recognitions == null || _image == null) return [];
@@ -95,10 +95,8 @@ class _PoseNetPageState extends State<PoseNet>
 
     _recognitions.forEach((re) {
       var list = re["keypoints"].values.map((k) {
-        if (k["score"] >= 0.5) // 정확도가 50% 이상이라면
-        {
-            lists.add({'part':k["part"], 'left':k["x"] * factorX - 6, 'top':k["y"] * factorY - 6, 'accuracy':k["score"]});
-            //          print(k["part"].toString() + ' x = ' + k["x"].toString() + ' y = ' + k["y"].toString() + ' accuracy = ' + k["score"].toString());
+        if (k["score"] >= 0.5) { // 정확도가 50% 이상이라면
+          lists.add({'part':k["part"], 'left':k["x"] * factorX - 6, 'top':k["y"] * factorY - 6, 'accuracy':k["score"]});
         }
       }).toList();
     });
@@ -106,23 +104,20 @@ class _PoseNetPageState extends State<PoseNet>
     return lists;
   }
 
+  // 전처리 (같은 항목 제거)
   List Repoints()
   {
     bool changed=false;
     List delete = [];
     List equal_delete = [];
     int ct = 0;
-    //print('시작할때'+points.length.toString());
-    for (int i =0; i< points.length-2; i++)
-    {
-      for (int j=i+1; j<points.length;j++)
-      {
+    for (int i =0; i< points.length-2; i++) {
+      for (int j=i+1; j<points.length;j++) {
         if (points[i]['part'] == points[j]['part'] && points[i]['accuracy'] == points[j]['accuracy'])
           equal_delete.add(j);
       }
     }
 
-    //print(equal_delete);
     equal_delete = equal_delete.toSet().toList();
     equal_delete.sort();
     int temp=0;
@@ -132,34 +127,22 @@ class _PoseNetPageState extends State<PoseNet>
       temp++;
     }
 
-    for (int i=0; i<points.length; i++)
-    {
-      for (int j=0; j<points.length; j++)
-      {
-        //print(points[i]["part"]+'와'+points[j]["part"]+'를 비교');
+    // 같은 포인트에 값이 2개 이상 있으면, 정확도가 높은 point만 살아남음
+    for (int i=0; i<points.length; i++) {
+      for (int j=0; j<points.length; j++) {
         if (i == j) continue;
 
-        else if ((points[i]['part'] == points[j]['part']))
-        {
+        else if ((points[i]['part'] == points[j]['part'])) {
           if (points[i]['accuracy'] > points[j]['accuracy'])
-          {
-            //points.removeAt(j);
-            //print(points[j]['part'] + points[i]['accuracy'].toString() + points[j]['accuracy'].toString());
             delete.add(j);
-          }
           else if (points[i]['accuracy'] < points[j]['accuracy'])
-          {
-            //points.removeAt(i);
-            //print(points[j]['part'] + points[i]['accuracy'].toString() + points[j]['accuracy'].toString());
             delete.add(i);
-          }
         }
       }
     }
+
     delete = delete.toSet().toList();
-//    print(delete);
     delete.sort();
-//    print(delete);
 
     temp=0;
     for (int i =0; i<delete.length; i++)
@@ -170,6 +153,7 @@ class _PoseNetPageState extends State<PoseNet>
     return points;
   }
 
+  // point를 표시해주는 위젯
   Widget pt(double x, double y, String part)
   {
     return Positioned(
@@ -188,11 +172,12 @@ class _PoseNetPageState extends State<PoseNet>
     );
   }
 
+  // Stack을 위한 paint 위젯
   Widget posePaint(double sx, double sy)
   {
     return CustomPaint(
       size: Size(sx, sy), // 위젯의 크기를 정함.
-      painter: MyPainter(posenetPoint:posenetPoint), // painter에 그리기를 담당할 클래스를 넣음.
+      painter: Painter(posenetPoint:posenetPoint), // painter에 그리기를 담당할 클래스를 넣음.
     );
   }
 
@@ -203,28 +188,24 @@ class _PoseNetPageState extends State<PoseNet>
     if (!(_image == null))
     {
       points = Keypoints(size);
-      print(points);
       posenetPoint = Repoints();
 
-      print(posenetPoint.length.toString());
-
-      posenetPoint.forEach((element)
-      {
+      posenetPoint.forEach((element) {
         print(element);
       });
       print(_imageWidth.toString() + 'x' +_imageHeight.toString());
 
+      // stack에 배경을 넣어줌
       stackChildren.add(backgr());
 
+      // point(점)을 찍어줌
       for (int i=0; i<posenetPoint.length; i++)
         stackChildren.add(pt(posenetPoint[i]['left'],posenetPoint[i]['top'],posenetPoint[i]['part']));
 
-      //stackChildren.addAll(makePoint(size));
+      // 선을 그려줌
       stackChildren.add(posePaint(_imageWidth, _imageHeight));
-
     }
 
-    //(_image == null) ? Text('d') : Image.file(File(_img.path)),
     return Scaffold(
       appBar: AppBar(
         title: Text('test'),
@@ -245,148 +226,3 @@ class _PoseNetPageState extends State<PoseNet>
     );
   }
 }
-
-class MyPainter extends CustomPainter {
-  List posenetPoint ; // 선을 그리기 위한 좌표값을 만듬.
-  MyPainter({Key key , @required this.posenetPoint});
-
-  double ax=5;
-  double ay=6;
-
-  @override
-  void paint(Canvas canvas, Size size)
-  {
-    Paint paint = Paint() // Paint 클래스는 어떤 식으로 화면을 그릴지 정할 때 쓰임.
-      ..color = Colors.deepPurpleAccent // 색은 보라색
-      ..strokeCap = StrokeCap.round // 선의 끝은 둥글게 함.
-      ..strokeWidth = 2.5; // 선의 굵기
-
-
-    for (int i=0; i<posenetPoint.length; i++)
-    {
-      switch (posenetPoint[i]['part'])
-      {
-        case 'leftShoulder':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'rightShoulder')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'leftElbow')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'leftHip')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-        case 'rightShoulder':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'leftShoulder')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'rightElbow')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'rightHip')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-        case 'leftElbow':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'leftWrist')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-        case 'rightElbow':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'rightWrist')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-        case 'leftHip':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'rightHip')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'leftShoulder')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'leftKnee')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-        case 'rightHip':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'leftHip')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'rightShoulder')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            else if (posenetPoint[j]['part'] == 'rightKnee')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-        case 'leftKnee':
-          {
-            for (int j=0; j<posenetPoint.length; j++)
-            {
-              if (posenetPoint[j]['part'] == 'leftAnkle')
-                canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                    Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-            }
-          }
-          break;
-
-        case 'rightKnee':
-        {
-          for (int j=0; j<posenetPoint.length; j++)
-          {
-            if (posenetPoint[j]['part'] == 'rightAnkle')
-              canvas.drawLine(Offset(posenetPoint[i]['left']+ax,posenetPoint[i]['top']+ay),
-                  Offset(posenetPoint[j]['left']+ax,posenetPoint[j]['top']+ay), paint); // 선을 그림.
-          }
-        }
-        break;
-
-      }
-
-    }
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-
-}
-
